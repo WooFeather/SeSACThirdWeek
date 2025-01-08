eec16c4376d1cceb7112dabdf0683c7f02aa3137
Woohyun Jo <bluewing1004@gmail.com>
2025-01-08 16:05:35 +0900

    pickerView, textFieldView, MapView 실습

--- SeSACThirdWeek/ViewController.swift
+++ SeSACThirdWeek/ViewController.swift
@@ -8,17 +8,75 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate, MKMapViewDelegate {
+    
     @IBOutlet var userTextField: UITextField!
     @IBOutlet var mapView: MKMapView!
-    @IBOutlet var pickerView: UIPickerView!
     @IBOutlet var secondTextField: UITextField!
+    
+    let pickerView = UIPickerView()
+    
+    let list = ["가", "나", "다"]
+    let array = ["1", "2", "3", "4"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        secondTextField.tintColor = .clear
+        secondTextField.inputView = pickerView
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        
+        userTextField.delegate = self
+        
+        mapView.delegate = self
+        
+        configureMapView()
     }
-
-
+    
+    func configureMapView() {
+        let center = CLLocationCoordinate2D(latitude: 37.65370, longitude: 127.04740)
+        mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 500, longitudinalMeters: 500)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        annotation.title = "창동티비"
+        mapView.addAnnotation(annotation)
+    }
+    
+    // 키보드에서 엔터키를 탭했을 때
+    // didEndOnExit과 다른점 => 얘는 키보드를 내리는거까지
+    // textFieldShouldReturn => 얘는 엔터키를 눌렀을 때
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == userTextField {
+            view.endEditing(true)
+        }
+        
+        return true
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return component == 0 ? list.count : array.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return component == 0 ? list[row] : array[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(component, row)
+        
+        if component == 0 {
+            userTextField.text = list[row]
+        } else {
+            secondTextField.text = array[row]
+        }
+    }
 }
 

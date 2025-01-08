cdc6bef20d60215c9dbe484e1d5edba32834ec76
Woohyun Jo <bluewing1004@gmail.com>
2025-01-08 15:11:28 +0900

    Initial Commit

---
+++ SeSACThirdWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACThirdWeek
+//
+//  Created by 조우현 on 1/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
